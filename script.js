bf21ad1822310276a2b74690d23a17e1e6dfb233
Matheus <matheuspedrosa307@gmail.com>
2024-11-04 21:24:03 -0300

    feat: função de alterar fonte

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const fontes = [
     "Arial, Helvetica, sans-serif",
     "'Courier New', Courier, monospace",
-    "'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif"
+    "'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif",
+    "'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif",
+    "'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif"
 ];
 
 let fonte = document.querySelector("#fontes");
 let texto = document.querySelector('#area-do-texto');
 
-texto.addEventListener('input', ()=> {
-    texto.style.fontFamily = "'Courier New', Courier, monospace";
+fonte.addEventListener('change', () => {
+    for (let i = 0; i < fontes.length; i++) {
+        if (fontes[i] == fonte.value) {
+            texto.style.fontFamily = fonte.value;
+        }
+    }
 })
