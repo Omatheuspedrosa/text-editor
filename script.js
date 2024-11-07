cb60f86b12b2696dc64beb23595d5360a30fbac8
Matheus <matheuspedrosa307@gmail.com>
2024-11-06 21:04:02 -0300

    descompliquei

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
-const fontes = [
-    "Arial, Helvetica, sans-serif",
-    "'Courier New', Courier, monospace",
-    "'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif",
-    "'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif",
-    "'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif"
-];
-
 let fonte = document.querySelector("#fontes");
 let texto = document.querySelector('#area-do-texto');
+let tamanhoDaFonte = document.querySelector(".tamanho-da-fonte .form-control")
 
 fonte.addEventListener('change', () => {
-    for (let i = 0; i < fontes.length; i++) {
-        if (fontes[i] == fonte.value) {
-            texto.style.fontFamily = fonte.value;
-        }
-    }
+    texto.style.fontFamily = fonte.value;
+})
+
+tamanhoDaFonte.addEventListener('change', () => {
+    texto.style.fontSize = tamanhoDaFonte.value + "px";
 })
