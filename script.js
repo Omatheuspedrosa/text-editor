3bbf9745831dad621a1cce6678f269af1fe75935
Matheus <matheuspedrosa307@gmail.com>
2024-11-10 14:33:57 -0300

    feat: funções de aumentar e diminuir o tamanho da fonte

--- script.js
+++ script.js
@@ -2,10 +2,28 @@ let fonte = document.querySelector("#fontes");
 let texto = document.querySelector('#area-do-texto');
 let tamanhoDaFonte = document.querySelector(".tamanho-da-fonte .form-control")
 
+function limitarTamanhoFonte(valor) {
+    return Math.max(1, Math.min(valor, 100));
+}
+
 fonte.addEventListener('change', () => {
     texto.style.fontFamily = fonte.value;
-})
+});
 
 tamanhoDaFonte.addEventListener('change', () => {
-    texto.style.fontSize = tamanhoDaFonte.value + "px";
-})
+    let novoTamanho = limitarTamanhoFonte(parseInt(tamanhoDaFonte.value) || 1);
+    tamanhoDaFonte.value = novoTamanho;
+    texto.style.fontSize = novoTamanho + "px";
+});
+
+document.querySelector(".btn-menos").addEventListener('click', () => {
+    let novoTamanho = limitarTamanhoFonte(parseInt(tamanhoDaFonte.value) - 1 || 1);
+    tamanhoDaFonte.value = novoTamanho;
+    texto.style.fontSize = novoTamanho + "px";
+});
+
+document.querySelector(".btn-mais").addEventListener('click', () => {
+    let novoTamanho = limitarTamanhoFonte(parseInt(tamanhoDaFonte.value) + 1 || 100);
+    tamanhoDaFonte.value = novoTamanho;
+    texto.style.fontSize = novoTamanho + "px";
+});
