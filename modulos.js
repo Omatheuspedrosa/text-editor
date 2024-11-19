b6e68636e8175a10e4d1e8e5ae43e5fd0a835c6a
Matheus <matheuspedrosa307@gmail.com>
2024-11-19 20:12:52 -0300

    farmando commit

--- modulos.js
+++ modulos.js
@@ -1,16 +1,18 @@
-export function limitarTamanhoFonte(valor) {
+function limitarTamanhoFonte(valor) {
     return Math.max(1, Math.min(valor, 100));
-}
+};
 
-export function transformarTexto(texto, elemento, classe) {
+function transformarTexto(texto, elemento, classe) {
     document.querySelector(elemento).addEventListener('click', () => {
         texto.classList.toggle(classe);
         document.querySelector(elemento).classList.toggle("ativo");
     });
-}
+};
 
-export function alinhamento(texto, elemento, posicao) {
+function alinhamento(texto, elemento, posicao) {
     document.querySelector(elemento).addEventListener('click', () => {
         texto.style.textAlign = posicao;
     })
-}
+};
+
+export {limitarTamanhoFonte, transformarTexto, alinhamento};
